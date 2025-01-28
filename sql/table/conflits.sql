2316d131ff8f93a87d4570e5c912630e788ba74b
Veillard Corentin <corentin.veillard@etu.unice.fr>
2025-01-28 14:14:20 +0100

    ajoute de check sur les conflits.sql

--- sql/table/conflits.sql
+++ sql/table/conflits.sql
@@ -1,13 +1,16 @@
-DROP TABLE IF EXISTS conflits;
 DROP TABLE IF EXISTS residents_conflits;
+DROP TABLE IF EXISTS conflits;
+
 CREATE TABLE conflits (
     id_conflit SERIAL PRIMARY KEY,
-    etat BOOLEAN DEFAULT FALSE,
+    etat BOOLEAN DEFAULT FALSE NOT NULL,
     titre VARCHAR(255) NOT NULL,
     description TEXT,
-    date_signalement DATE DEFAULT CURRENT_DATE
+    date_signalement DATE DEFAULT CURRENT_DATE NOT NULL,
+    CHECK (char_length(titre) > 1),
+    CHECK (description IS NULL OR char_length(description) >= 10),
+    CHECK (date_signalement <= CURRENT_DATE)
 );
-
 
 CREATE TABLE residents_conflits (
     id_resident INT NOT NULL,
