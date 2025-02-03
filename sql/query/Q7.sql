6dd59c9922f1410281c02c9a5f6cf4f8c2897f02
Martini Alexis <alexismartini244@gmail.com>
2025-02-03 22:15:10 +0100

    fix #17

--- sql/query/Q7.sql
+++ sql/query/Q7.sql
@@ -4,14 +4,14 @@ SELECT
 
     -- Sous-requête pour calculer le nombre moyen d'équipements par logement pour ce type
     round((
-        SELECT AVG(sub.nb_equipements::numeric)
+        SELECT AVG(somme.nb_equipements::numeric)
         FROM (
                  SELECT COUNT(le2.id_equipement) AS nb_equipements
                  FROM logements l2
                           LEFT JOIN logements_equipements le2 ON l2.id_logement = le2.id_logement
                  WHERE l2.id_type_logement = tl.id_type_logement
                  GROUP BY l2.id_logement
-             ) sub
+             ) somme
     ),2) AS nb_equipements_moyen,
     -- Sous-requête pour calculer le prix moyen d'une nuit pour ce type de logement
     round((
