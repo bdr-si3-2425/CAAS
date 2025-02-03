50494db919207a22fc7a66b2c013c3b9666d99fd
Essalah Sabra <sabra.essalah@etu.unice.fr>
2025-02-03 15:00:55 +0100

    Modification de la table conflit pour changer la nmination d'un attribu 'reglé' en 'resolu' #10

--- python/main.py
+++ python/main.py
@@ -192,7 +192,7 @@ def generate_conflits_inserts(num_conflicts):
 )""")
 
     return f"""
-INSERT INTO conflits (reglé, titre, description, date_signalement)
+INSERT INTO conflits (resolu, titre, description, date_signalement)
 VALUES {','.join(values)};"""
 
 def generate_residents_conflits_inserts(num_links):
