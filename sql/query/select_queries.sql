372f9462849c7a29fb029e9f267f15c81f2c31d1
Essalah Sabra <sabra.essalah@etu.unice.fr>
2025-02-04 08:26:21 +0100

    Modification de l'appel du fichier de la vue resident dates #15

--- sql/query/select_queries.sql
+++ sql/query/select_queries.sql
@@ -1,6 +1,6 @@
 -- 1. Quels logements sont disponibles pour une période donnée, selon des critères spécifiques (type, emplacement, prix) ?
 
--- Exemples de select :
+-- Exemples de select Pour avoir les logements disponibles à une période donnée :
 SELECT *
 FROM get_logements_disponibles(
         '2025-07-01',
